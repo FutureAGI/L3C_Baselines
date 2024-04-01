a18981224c4908ae9992ea91d399d0e37e87e5e2
wangfan04 <wangfan04@baidu.com>
2024-04-01 10:24:25 +0800

    update

---
+++ data/README.md
@@ -0,0 +1,21 @@
+# L3C_Baselines/data
+Generate the benchmark data for l3c baselines
+
+## Usage
+```shell
+python gen_maze_record.py 
+  --output_path OUTPUT_PATH
+  --task_type TASK_TYPE
+  --maze_type MAZE_TYPE
+  --max_steps MAX_STEPS
+  --n n1,n2,n3
+  --density d1,d2,d3
+  --n_landmarks l1,l2,l3
+  --epochs EPOCHS
+  --workers WORKERS
+```
+
+## Output
+```
+bash gen_metalm_data.sh
+```
--- data/gen_maze_record.py
+++ data/gen_maze_record.py
@@ -74,24 +74,21 @@ def create_directory(directory_path):
     if not os.path.exists(directory_path):
         os.makedirs(directory_path)
 
-def dump_maze(file_name, epoch_ids, n_list, n_landmarks_list, density_list, maze_type, max_steps, task_type):
+def dump_maze(path_name, epoch_ids, n_list, n_landmarks_list, density_list, maze_type, max_steps, task_type):
     for idx in epoch_ids:
         n = random.choice(n_list)
         n_landmarks = random.choice(n_landmarks_list)
         density = random.choice(density_list)
 
         observations, actions, rewards, maps = run_maze_epoch(
-                maze_type=args.maze_type,
-                max_steps=args.max_steps,
+                maze_type=maze_type,
+                max_steps=max_steps,
                 n=n,
-                task_type=args.task_type,
+                task_type=task_type,
                 density=density,
                 n_landmarks=n_landmarks)
 
-        if(args.epochs > 1):
-            file_path = "%s-%04d"%(file_name, idx)
-        else:
-            file_path = file_name
+        file_path = f'{path_name}/record-{idx:04d}'
 
         # Convert observations, actions, and rewards to lmdb format and save file
         # Open the lmdb environment
@@ -104,20 +101,20 @@ def dump_maze(file_name, epoch_ids, n_list, n_landmarks_list, density_list, maze
 if __name__=="__main__":
     # Parse the arguments, should include the output file name
     parser = argparse.ArgumentParser()
-    parser.add_argument("--output", type=str, default="maze_data", help="output file name")
-    parser.add_argument("--task_type", type=str, default="NAVIGATION", help="task type")
-    parser.add_argument("--maze_type", type=str, default="Discrete3D", help="maze type")
-    parser.add_argument("--max_steps", type=int, default=4000, help="max steps")
-    parser.add_argument("--n", type=str, default="9,15,25,35", help="n:a list of int")
+    parser.add_argument("--output_path", type=str, default="./maze_data/", help="output directory, the data would be stored as output_path/record-xxxx.npy")
+    parser.add_argument("--task_type", type=str, default="NAVIGATION", help="task type, NAVIGATION/SURVIVAL, default:NAVIGATION")
+    parser.add_argument("--maze_type", type=str, default="Discrete3D", help="maze type, Discrete2D/Discrete3D/Continuous3D, default:Discrete3D")
+    parser.add_argument("--max_steps", type=int, default=4000, help="max steps, default:4000")
+    parser.add_argument("--scale", type=str, default="9,15,25,35", help="a list of scales separated with comma to randomly choose")
     parser.add_argument("--density", type=str, default="0.20,0.34,0.36,0.38,0.45", help="density:a list of float")
-    parser.add_argument("--n_landmarks", type=str, default="5,6,7,8,9,10", help="n_landmarks:a list of int")
+    parser.add_argument("--landmarks", type=str, default="5,6,7,8,9,10", help="landmarks:a list of number of landmarks")
     parser.add_argument("--epochs", type=int, default=1, help="multiple epochs:default:1")
     parser.add_argument("--workers", type=int, default=4, help="number of multiprocessing workers")
     args = parser.parse_args()
 
     density_list = list(map(float, args.density.split(",")))
-    n_list = list(map(int, args.n.split(",")))
-    n_landmarks_list = list(map(int, args.n_landmarks.split(",")))
+    n_list = list(map(int, args.scale.split(",")))
+    n_landmarks_list = list(map(int, args.landmarks.split(",")))
 
     worker_splits = args.epochs / args.workers + 1.0e-6
     processes = []
@@ -129,7 +126,7 @@ if __name__=="__main__":
 
         print("start processes generating %04d to %04d" % (n_b, n_e))
         process = multiprocessing.Process(target=dump_maze, 
-                args=(args.output, range(n_b, n_e), n_list, n_landmarks_list, density_list, args.maze_type, args.max_steps, args.task_type))
+                args=(args.output_path, range(n_b, n_e), n_list, n_landmarks_list, density_list, args.maze_type, args.max_steps, args.task_type))
         processes.append(process)
         process.start()
 
