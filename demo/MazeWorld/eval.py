8a8e8eaa68755ba9c6757f2d3b0b60a983184f10
WorldEditors <>
2024-04-16 16:21:13 +0800

    update

--- demo/MazeWorld/eval.py
+++ demo/MazeWorld/eval.py
@@ -9,15 +9,18 @@ import cv2
 import torch.optim as optim
 import torch.distributed as dist
 import torch.multiprocessing as mp
-from l3c.mazeworld import MazeTaskSampler
 from torch.optim.lr_scheduler import CosineAnnealingLR
 from torch.nn.parallel import DistributedDataParallel as DDP
 from torch.nn.parallel import DataParallel as DP
 from torch.utils.data import DataLoader, Dataset
 
+from l3c.mazeworld import MazeTaskSampler
+from l3c.mazeworld.agents import SmartSLAMAgent
+
 from dataloader import MazeDataSet
 from models import MazeModelBase
 from utils import VideoWriter
+
 
 def postprocess_image(img, scale_factor, texts):
     w, h, c = img.shape
@@ -33,7 +36,7 @@ action_texts = ["STOP", "TURN LEFT", "TURN RIGHT", "BACKWARD", "FORWARD"]
 def action_text(action):
     return "Actions:" + action_texts[action]
 
-def demo_epoch(maze_env, task, model, device, video_writer):
+def model_epoch(maze_env, task, model, device, video_writer=None):
     # Example training loop
     maze_env.set_task(task)
     obs = maze_env.reset()
@@ -72,35 +75,47 @@ def demo_epoch(maze_env, task, model, device, video_writer):
         obs_err_rec = numpy.sqrt(numpy.mean((rec_obs_pred - obs_arr[-2]) ** 2))
         loc_map = cv2.resize(loc_map, (128, 128), interpolation=cv2.INTER_NEAREST)
 
-        line1 = numpy.transpose(numpy.concatenate([obs_arr[-2], rec_obs_pred, obs_arr[-1], loc_map], axis=0), (1, 0, 2))
-        line2 = numpy.transpose(numpy.concatenate(next_obs_pred_list[-4:], axis=0), (1, 0, 2))
-        raw_img = numpy.concatenate([line1, line2], axis=0)
-        img = postprocess_image(raw_img, 3, (
-            ((10, 10), "Observation t"),
-            ((20, 64), action_text(act_arr[-1])),
-            ((138, 10), "Reconstruction t"),
-            ((266, 10), "Observation t + 1"),
-            ((394, 10), "Local Map"),
-            ((10, 138), "Predict t+1 x_T"),
-            ((138, 138), "Predict t+1 x_2T/3"),
-            ((266, 138), "Predict t+1 x_T/3"),
-            ((394, 138), "Predict t+1 x_0"),
-            ))
-        
-        video_writer.add_image(img)
+        if(video_writer is not None):
+            line1 = numpy.transpose(numpy.concatenate([obs_arr[-2], rec_obs_pred, obs_arr[-1], loc_map], axis=0), (1, 0, 2))
+            line2 = numpy.transpose(numpy.concatenate(next_obs_pred_list[-4:], axis=0), (1, 0, 2))
+            raw_img = numpy.concatenate([line1, line2], axis=0)
+            img = postprocess_image(raw_img, 3, (
+                ((10, 10), "Observation t"),
+                ((20, 64), action_text(act_arr[-1])),
+                ((138, 10), "Reconstruction t"),
+                ((266, 10), "Observation t + 1"),
+                ((394, 10), "Local Map"),
+                ((10, 138), "Predict t+1 x_T"),
+                ((138, 138), "Predict t+1 x_2T/3"),
+                ((266, 138), "Predict t+1 x_T/3"),
+                ((394, 138), "Predict t+1 x_0"),
+                ))
+            
+            video_writer.add_image(img)
 
-        print("Step: %d, Reward: %f, Reward Summary: %f, Observation Prediction Error: %f, Reconstruction Error: %f" % (step, next_rew_gt, rew_sum, obs_err, obs_err_rec))
+            print("Step: %d, Reward: %f, Reward Summary: %f, Observation Prediction Error: %f, Reconstruction Error: %f" % (step, next_rew_gt, rew_sum, obs_err, obs_err_rec))
 
         sys.stdout.flush()
+    return rew_sum
 
-    obs_tor = torch.from_numpy(numpy.array(obs_arr)).float().to(device).unsqueeze(0)
-    act_tor = torch.from_numpy(numpy.array(act_arr)).long().to(device).unsqueeze(0)
-    obs_tor = obs_tor.permute(0, 1, 4, 2, 3)
-    with torch.no_grad():
-        rec_obs, next_obs_list, next_act, cache = model.inference_next(obs_tor, act_tor)
-        next_action = int(next_act.squeeze().item())
-    print(next_action)
 
+def agent_epoch(maze_env, task):
+    # Example training loop
+    maze_env.set_task(task)
+    observation = maze_env.reset()
+    agent = SmartSLAMAgent(maze_env=maze_env, render=True)
+
+    done = False
+    step = 0
+    cache = None
+    rew_sum = 0
+    reward = 0
+    while not done:
+        step += 1
+        action = agent.step(observation, reward)
+        observation, reward, done, _ = maze_env.step(action)
+        rew_sum += reward
+    return rew_sum
 
 if __name__=='__main__':
     parser = argparse.ArgumentParser()
@@ -109,18 +124,10 @@ if __name__=='__main__':
     parser.add_argument('--test_time_step', type=int, default=256)
     parser.add_argument('--scale', type=int, default=15)
     parser.add_argument('--density', type=int, default=0.36)
+    parser.add_argument('--test_epochs', type=int, default=1)
     parser.add_argument('--n_landmarks', type=int, default=8)
     parser.add_argument('--output', type=str, default="./videos")
     args = parser.parse_args()
-
-    maze_env = gym.make("mazeworld-discrete-3D-v1", enable_render=False, max_steps=args.test_time_step, task_type="NAVIGATION", resolution=(128, 128))
-
-    task = MazeTaskSampler(n=args.scale, allow_loops=True, 
-            wall_density=args.density,
-            landmarks_number=args.n_landmarks,
-            landmarks_avg_reward=0.5,
-            commands_sequence = 10000,
-            verbose=False)
 
     model = MazeModelBase(image_size=128, map_size=7, action_size=5, max_time_step=args.max_time_step)
     use_gpu = torch.cuda.is_available()
@@ -132,6 +139,27 @@ if __name__=='__main__':
     DP(model).load_state_dict(torch.load('%s/model.pth' % args.load_path))
     model = model.to(device)
 
-    video_writer = VideoWriter("./videos", "demo", window_size=(1536, 768))
-    demo_epoch(maze_env, task, model, device, video_writer)
+    reward_model = []
+    reward_agent = []
+
+    for idx in range(args.test_epochs):
+
+        maze_env = gym.make("mazeworld-discrete-3D-v1", enable_render=False, max_steps=args.test_time_step, task_type="NAVIGATION", resolution=(128, 128))
+
+        task = MazeTaskSampler(n=args.scale, allow_loops=True, 
+                wall_density=args.density,
+                landmarks_number=args.n_landmarks,
+                landmarks_avg_reward=0.5,
+                commands_sequence = 10000,
+                verbose=False)
+
+        video_writer = VideoWriter("./videos", "demo", window_size=(1536, 768))
+        reward_model.append(model_epoch(maze_env, task, model, device, video_writer))
+        maze_env.save_trajectory(f'./videos/traj_model_{idx}.jpg')
+
+        reward_agent.append(agent_epoch(maze_env, task))
+        maze_env.save_trajectory(f'./videos/traj_agent_{idx}.jpg')
+        print("Model Reward: %s ; Agent Reward: %s"%(reward_model[-1], reward_agent[-1]))
+
     video_writer.clear()
+    print("Model Reward: %s ; Agent Reward: %s"%(numpy.mean(reward_model), numpy.mean(reward_agent)))
