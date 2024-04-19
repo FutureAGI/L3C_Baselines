1cda081aa53c1ad34278559284e9417f394c6aff
root <root@szzj-inf-sci-k8s-a800-hbxgn6-0077.szzj.baidu.com>
2024-04-19 20:43:18 +0800

    update

--- data/dump_maze.sh
+++ data/dump_maze.sh
@@ -1,11 +1,10 @@
-python gen_lm_data.py \
-  --version v2 \
-  --vocab_size 32,64,128 \
-  --embedding_size 16,32 \
-  --hidden_size 16,32,64 \
-  --n_gram 2,3,4,5 \
-  --sequence_length 8192 \
-  --file_size 500 \
-  --file_number 1000 \
-  --workers 32 \
   --output_path /root/workspace/afs/lm_data_train/
+
+python gen_maze_record.py \
+  --output_path /root/workspace/ \
+  --task_type NAVIGATION \
+  --maze_type Discrete3D \
+  --max_steps 2048 \
+  --n 9,15,21,25 \
+  --epochs 1000 \
+  --workers 32
