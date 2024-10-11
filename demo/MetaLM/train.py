c2f482870f93c655af193de6c046afc39114818d
Wang Fan (AIRS) <wangfan@cuhk.edu.cn>
2024-10-11 17:27:30 +0800

    update

--- demo/MetaLM/train.py
+++ demo/MetaLM/train.py
@@ -11,11 +11,11 @@ from torch.nn.parallel import DistributedDataParallel as DDP
 from torch.nn.utils import clip_grad_norm_
 from torch.utils.data import DataLoader, Dataset
 from torch.cuda.amp import autocast, GradScaler
-from dataloader import LMDataSet
-from utils import custom_load_model, noam_scheduler, LinearScheduler
-from utils import show_bar, count_parameters, model_path
-from utils import Configure, gradient_failsafe
-from models import LMBase
+from l3c_baselines.dataloader import LMDataSet
+from l3c_baselines.utils import custom_load_model, noam_scheduler, LinearScheduler
+from l3c_baselines.utils import show_bar, count_parameters, model_path
+from l3c_baselines.utils import Configure, gradient_failsafe
+from l3c_baselines.models import LanguageModel
 from restools.logging import Logger, log_progress, log_debug, log_warn
 
 os.environ['MASTER_ADDR'] = 'localhost'  # Example IP address, replace with your master node's IP
@@ -40,10 +40,7 @@ def main_epoch(rank, use_gpu, world_size, config, main_rank):
         print("Main gpu", use_gpu, "rank:", rank, device)
 
     # Create model and move it to GPU with id `gpu`
-    model = LMBase(config.model_config)
-                
-    log_debug("Number of parameters: ", count_parameters(model), on=main)
-
+    model = LanguageModel(config.model_config, verbose=main)
 
     model = model.to(device)
 
@@ -54,29 +51,24 @@ def main_epoch(rank, use_gpu, world_size, config, main_rank):
 
     # Example dataset and dataloader
     train_config = config.train_config
-    load_model_path = train_config.load_model_path
-    learning_rate = train_config.learning_rate
-    time_step = train_config.time_step
-    noam_decay_interval = train_config.learning_rate_noam_decay_interval
-    batch_size = config.train_config.batch_size
-    max_epochs = train_config.max_epochs
-    eval_interval = train_config.evaluation_interval
-    use_amp = train_config.use_amp
-    use_scaler = train_config.use_scaler
-    train_start_step = train_config.start_step
+    test_config = config.test_config
 
     dataset = LMDataSet(train_config.data_path, train_config.file_size, verbose=main)
     sampler = torch.utils.data.distributed.DistributedSampler(dataset, num_replicas=world_size, rank=rank)
-    dataloader = DataLoader(dataset, batch_size=batch_size, sampler=sampler)
+    dataloader = DataLoader(dataset, batch_size=train_config.batch_size, sampler=sampler)
 
     if(main):
         logger = Logger("iteration", "learning_rate", "perplexity", sum_iter=len(dataloader), use_tensorboard=True)
 
-    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
-    scheduler = LambdaLR(optimizer, lr_lambda=lambda x:noam_scheduler(x, noam_decay_interval))
+    optimizer = torch.optim.Adam(model.parameters(), lr=train_config.lr)
+    scheduler = LambdaLR(optimizer, lr_lambda=lambda x:noam_scheduler(x, train_config.lr_decay_interval))
 
-    if(load_model_path is not None):
-        model = custom_load_model(model, f'{load_model_path}/model.pth', strict_check=False)
+    if(train_config.has_attr("load_model_path") and 
+            train_config.load_model_path is not None and 
+            train_config.load_model_path.lower() != 'none'):
+        model = custom_load_model(model, 
+                                  f'{train_config.load_model_path}/model.pth', 
+                                  strict_check=False)
 
     # Perform the first evaluation
     test_config = config.test_config
@@ -86,86 +78,88 @@ def main_epoch(rank, use_gpu, world_size, config, main_rank):
 
     def main_round(rid, dataloader):
         total_iteration = len(dataloader)
+
         for batch_idx, (feature, label) in enumerate(dataloader):
-            feature = feature[:, :time_step].to(device)
-            label = label[:, :time_step].to(device)
-            optimizer.zero_grad()
-            with autocast(dtype=torch.float16, enabled=use_amp):
-                loss = model.module.perplexity(feature, label)
+            acc_iter += 1
+            # Important: Must reset the model before each segment
+            model.module.reset()
+            start_position = 0
+            for sub_idx, fea, lab in segment_iterator(
+                            train_config.seq_len, train_config.seg_len, device, feature, label):
+                fea = fea.to(device)
+                lab = lab.to(device)
+                optimizer.zero_grad()
+                with autocast(dtype=torch.bfloat16, enabled=use_amp):
+                    loss = model.module.perplexity(fea, lab, start_position=start_position)
+                start_position += fea.shape[1]
 
-            if(use_scaler):
-                scaler.scale(loss).backward()
-                gradient_failsafe(model.module, optimizer, scaler)
-                clip_grad_norm_(model.module.parameters(), 1.0)
-                scaler.step(optimizer)
-                scaler.update()
-            else:
-                loss.backward()
-                optimizer.step()
+                if(use_scaler):
+                    scaler.scale(loss).backward()
+                    gradient_failsafe(model.module, optimizer, scaler)
+                    clip_grad_norm_(model.module.parameters(), 1.0)
+                    scaler.step(optimizer)
+                    scaler.update()
+                else:
+                    loss.backward()
+                    optimizer.step()
 
-            scheduler.step()
+                scheduler.step()
 
-            if(main):
-                logger.log(batch_idx, scheduler.get_last_lr()[0], float(loss.detach().cpu().numpy()), iteration=batch_idx, epoch=epoch_id)
+                if(main):
+                    logger.log(batch_idx, 
+                               scheduler.get_last_lr()[0], 
+                               float(loss.detach().cpu().numpy()), 
+                               iteration=batch_idx, 
+                               epoch=epoch_id)
 
     # Example training loop
-    for epoch_id in range(1, max_epochs + 1):
+    for epoch_id in range(1, train_config.max_epochs + 1):
         model.train()
         main_round(epoch_id, dataloader)
-        if(main and epoch_id % eval_interval == 0):
+        if(main and epoch_id % train_config.eval_interval == 0):
             mod_path, opt_path_vae, opt_path_seq = model_path(train_config.save_model_path, epoch_id)
             torch.save(model.state_dict(), mod_path)
-            torch.save(optimizer.state_dict(), opt_path_seq)
+            #torch.save(optimizer.state_dict(), opt_path_seq)
 
         model.eval()
         # Perform the evaluation according to interval
-        if(epoch_id % eval_interval == 0):
+        if(epoch_id % train_config.eval_interval == 0):
             test_epoch(rank, use_gpu, world_size, test_config, model, main, device, epoch_id)
 
 def test_epoch(rank, use_gpu, world_size, config, model, main, device, epoch_id):
     # Example training loop
 
-    data_path = config.data_path
-    file_size = config.file_size
-    batch_size = config.batch_size
-    time_step = config.time_step
     results = []
-    dataset = LMDataSet(data_path, file_size, verbose=main)
+    dataset = LMDataSet(config.data_path, config.file_size, verbose=main)
     sampler = torch.utils.data.distributed.DistributedSampler(dataset, num_replicas=world_size, rank=rank)
-    dataloader = DataLoader(dataset, batch_size=batch_size, sampler=sampler)
+    dataloader = DataLoader(dataset, batch_size=config.batch_size, sampler=sampler)
     all_length = len(dataloader)
 
     log_debug("[EVALUATION] Epochs: %s..." % epoch_id, on=main)
+    stat = DistStatistics("perplexity", "count")
 
     for batch_idx, (feature, label) in enumerate(dataloader):
-        feature = feature[:, :time_step].to(device)
-        label = label[:, :time_step].to(device)
-        with torch.no_grad():
-            loss = model.module.perplexity(feature, label)
-            length = torch.tensor(feature.shape[0] * feature.shape[1]).to(loss.device)
+        model.module.reset()
+        start_position = 0
+        for sub_idx, fea, lab in segment_iterator(
+                        config.seq_len, config.seg_len, device, feature, label):
+            fea = fea[:, :time_step].to(device)
+            lab = lab[:, :time_step].to(device)
+            with torch.no_grad():
+                loss = model.module.perplexity(fea, lab)
+                cnt = torch.tensor(fea.shape[0] * fea.shape[1]).to(loss.device)
 
-            loss = loss * length
+            stat.add_with_safty(rank, 
+                                perplexity=loss, 
+                                count=cnt)
 
-        dist.all_reduce(loss.data)
-        dist.all_reduce(length.data)
-
-        results.append((loss.cpu(), length.cpu()))
-
-        if(main):
-            log_progress((batch_idx + 1) / all_length)
-
-    sum_loss = 0
-    sum_cnt = 0
-    for loss, cnt in results:
-        sum_loss += loss
-        sum_cnt += cnt
-    sum_cnt = max(1, sum_cnt)
-    sum_loss /= sum_cnt
+            if(main):
+                log_progress((batch_idx + 1) / all_length)
 
     if(main):
-        log_debug("\n[EVALUATION] Epochs: %s; Perplexity: %s\n"
-                % (epoch_id, sum_loss))
-        sys.stdout.flush()
+        stat_res = stat()
+        logger = Logger(*stat_res.keys())
+        logger.log(*stat_res.values(), epoch=epoch_id)
 
 if __name__=='__main__':
     parser = argparse.ArgumentParser()
