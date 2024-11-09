f31bf3de1f19eab7da7905a8c3f98c6e4f4a3229
WorldEditors <fanwang.px@gmail.com>
2024-11-09 09:25:53 +0800

    update

--- data/dump_anymdp.sh
+++ data/dump_anymdp.sh
@@ -7,6 +7,4 @@
 	 --max_steps 16000 \
 	 --epochs 256 \
 	 --workers 64 \
-	 --behavior_noise_config 1.0,-1.0 1.0,-0.75 1.0,-0.5 1.0,-0.25 1.0,0.0 1.0,0.5 1.0,0.75 \
-	 --behavior_config 1.0,0.0 0.5,0.5 0.0,1.0 \
-	 --reference_config 1.0,0.0 0.5,0.5 0.2,0.8
+     --offpolicy_labeling 1
