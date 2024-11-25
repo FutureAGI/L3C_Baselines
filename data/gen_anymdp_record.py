a2c2513769848ff2ddaa7fe1833e83fab6f1e7fd
WorldEditors <fanwang.px@gmail.com>
2024-11-26 00:57:44 +0800

    update

--- data/gen_anymdp_record.py
+++ data/gen_anymdp_record.py
@@ -200,7 +200,7 @@ if __name__=="__main__":
     parser.add_argument("--task_file", type=str, default=None, help="Task source file, used if task_source = FILE")
     parser.add_argument("--state_num", type=int, default=128, help="state num, default:128")
     parser.add_argument("--action_num", type=int, default=5, help="action num, default:5")
-    parser.add_argument("--min_state_space", type=int, default=32, help="minimum state dim in task, default:8")
+    parser.add_argument("--min_state_space", type=int, default=16, help="minimum state dim in task, default:8")
     parser.add_argument("--max_steps", type=int, default=4000, help="max steps, default:4000")
     parser.add_argument("--offpolicy_labeling", type=int, default=0, help="enable offpolicy labeling (DAgger), default:False")
     parser.add_argument("--epochs", type=int, default=1, help="multiple epochs:default:1")
