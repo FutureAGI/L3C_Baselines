80d293160274fe46ce6d307775b92a61cf55d449
WorldEditor <fanwang.px@gmail.com>
2025-01-17 11:58:01 +0800

    update

--- airsoul/utils/__init__.py
+++ airsoul/utils/__init__.py
@@ -1,10 +1,11 @@
 from .losses import weighted_loss, parameters_regularization
 from .scheduler import LinearScheduler, noam_scheduler
-from .video_writer import create_folder, VideoWriter
+from .video_writer import VideoWriter
 from .stats import DistStatistics
 from .data_proc import rewards2go, img_pro, img_post, downsample, sa_dropout
 from .tools import model_path, safety_check, count_parameters,  format_cache, memory_cpy, check_model_validity, custom_load_model, apply_gradient_safely
 from .tools import Configure, Logger, log_warn, log_debug, log_progress, log_fatal
+from .tools import create_folder, import_with_caution
 from .trainer import EpochManager, Runner
 from .generator import GeneratorRunner, GeneratorBase
 from .vocab import tag_vocabulary, tag_mapping_gamma, tag_mapping_id
