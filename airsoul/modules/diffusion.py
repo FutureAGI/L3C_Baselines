a423b8a4d9909004fd3c4a1f8aa611e4149dbc4c
shaopengtao <shaopengtao@cuhk.edu.cn>
2025-02-27 16:14:21 +0800

    change cosine schedule method

--- airsoul/modules/diffusion.py
+++ airsoul/modules/diffusion.py
@@ -61,12 +61,7 @@ class DiffusionLayers(nn.Module):
             self.betas = torch.linspace(config.beta[0], config.beta[1], config.T)
             self.betas = torch.cat([torch.tensor([0.0]), self.betas], dim=0)
         elif config.schedule == "cosine":
-            betas = []
-            for i in range(config.T):
-                t1 = i / config.T
-                beta_t = min(config.beta[1], (1 - math.cos(math.pi * t1)) / 2 * (config.beta[1] - config.beta[0]) + config.beta[0])
-                betas.append(beta_t)
-            self.betas = torch.tensor(betas, dtype=torch.float32)
+            self.betas = betas_for_alpha_bar(config.T)
             self.betas = torch.cat([torch.tensor([0.0]), self.betas], dim=0)
         elif config.schedule == "scaled_linear":
             # this schedule is very specific to the latent diffusion model.
@@ -418,3 +413,46 @@ class FinalLayer(nn.Module):
 
 def modulate(x, shift, scale):
     return x * (1 + scale) + shift
+
+def betas_for_alpha_bar(
+    num_diffusion_timesteps,
+    max_beta=0.999,
+    alpha_transform_type="cosine",
+):
+    """
+    Create a beta schedule that discretizes the given alpha_t_bar function, which defines the cumulative product of
+    (1-beta) over time from t = [0,1].
+
+    Contains a function alpha_bar that takes an argument t and transforms it to the cumulative product of (1-beta) up
+    to that part of the diffusion process.
+
+
+    Args:
+        num_diffusion_timesteps (`int`): the number of betas to produce.
+        max_beta (`float`): the maximum beta to use; use values lower than 1 to
+                     prevent singularities.
+        alpha_transform_type (`str`, *optional*, default to `cosine`): the type of noise schedule for alpha_bar.
+                     Choose from `cosine` or `exp`
+
+    Returns:
+        betas (`np.ndarray`): the betas used by the scheduler to step the model outputs
+    """
+    if alpha_transform_type == "cosine":
+
+        def alpha_bar_fn(t):
+            return math.cos((t + 0.008) / 1.008 * math.pi / 2) ** 2
+
+    elif alpha_transform_type == "exp":
+
+        def alpha_bar_fn(t):
+            return math.exp(t * -12.0)
+
+    else:
+        raise ValueError(f"Unsupported alpha_transform_type: {alpha_transform_type}")
+
+    betas = []
+    for i in range(num_diffusion_timesteps):
+        t1 = i / num_diffusion_timesteps
+        t2 = (i + 1) / num_diffusion_timesteps
+        betas.append(min(1 - alpha_bar_fn(t2) / alpha_bar_fn(t1), max_beta))
+    return torch.tensor(betas, dtype=torch.float32)
